e6e5d2e845de8cf4982ef006e2f185c4d3329017
prep0ster0us <rbabu1@unh.newhaven.edu>
2023-09-26 18:02:34 -0400

    itemCollection source clean up

--- ItemCollection.cpp
+++ ItemCollection.cpp
@@ -5,30 +5,10 @@
 #include "Item.hpp"
 #include"ItemCollection.hpp"
 
-void ItemCollection::print() {
-    for(int i=0; i<itemNum; i++) {
-        itemList[i].print();
-    }
-}
-
 void ItemCollection::add(Item obj) {
     if(itemNum < MAX_ITEMS) {
         itemList[itemNum] = obj;
         ++itemNum;
-    }
-}
-
-
-void studentCollection::print() {
-    for(int i=0; i<cnt; i++) {
-        list[i].print();
-    }
-}
-
-void studentCollection::add(Student obj) {
-    if(cnt < MAX_STUD) {
-        list[cnt] = obj;
-        ++cnt;
     }
 }
 
@@ -46,6 +26,7 @@ void ItemCollection::findById(unsigned long id) {
 void ItemCollection::findByName(string name) {
     for(int i=0; i<itemNum; i++) {
         if(itemList[itemNum].getName().compare(name)==0) {
+            cout << "Here are the details for the item you are looking for:-" << endl;
             itemList[itemNum].print();
             return;
         }
